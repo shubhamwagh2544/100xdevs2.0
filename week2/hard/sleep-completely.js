10cad35b705a570bef0323e5bc7f40b8949e4c37
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-21 15:21:56 +0530

    blocking thread without performming async ops

---
+++ week2/hard/sleep-completely.js
@@ -0,0 +1,18 @@
+/*
+ * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
+ * During this time the thread should not be able to do anything else.
+ * the function should return a promise just like before
+ */
+
+function sleep(milliseconds) {
+    return new Promise((res, rej) => {
+        // stop for 3 sec
+        const start = Date.now();
+        while (Date.now() - start < milliseconds) {
+            // do nothing
+        }
+        res();
+    });
+}
+
+sleep(3000).then(() => console.log("Done"));
