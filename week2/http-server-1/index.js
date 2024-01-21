d930d061dfaa1ebf118ec1dab90df60f8355a3f7
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-21 09:27:04 +0530

    exposing services to end user

--- week2/http-server-1/index.js
+++ week2/http-server-1/index.js
@@ -4,12 +4,32 @@ const port = 3000;
 
 const app = express();
 
+function sum(n) {
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+        result += i;
+    }
+    return result;
+}
+
 app.get("/", function (req, res) {
+    /*
     let filePath = path.join(__dirname, "./design.html");
     // what is __dirname?
     console.log(__dirname);
     //res.sendFile(filePath);
-    res.send("<h1>hello world</h1>");
+    //res.send("<h1>hello world</h1>");
+    //res.send("Hello World");
+    const user = {
+        name: "John",
+        age: 20
+    };
+    res.send(user);
+    */
+
+    const n = req.query.n;
+    const result = sum(n);
+    res.send(`Result is ${result}`);
 });
 
 app.listen(port, function () {
