21d981bf364b41dc255efc5c123d612550acecd0
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-23 10:08:31 +0530

    global catches

---
+++ week3/global-catch/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    const kidneys = req.body.kidneys;
+    
+    // this will crash the server if kidneys is undefined or not an array
+    console.log(kidneys.length);
+    
+    res.send(`I have ${kidneys.length} kidneys`);
+})
+
+// if input valiadation fails, the server will crash
+// and the user will see a 500 error with error details
+// to fix this, we can use a global catch all error handler
+app.use(function(err, req, res, next) {
+    res
+    .status(500)
+    .send("Sorry! Something went wrong.")
+})
+
+
+app.listen(3000);
