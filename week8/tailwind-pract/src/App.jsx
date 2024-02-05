ef1a983e0df5bffb137f029f8a7a14a2f3061fbf
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-05 21:25:13 +0530

    tailwind start-dummy components

---
+++ week8/tailwind-pract/src/App.jsx
@@ -0,0 +1,17 @@
+import { Flex, Grid, Responsive, Responsive1 } from "./pract components/PracticeComponents"
+
+function App() {
+
+  return (
+    <>
+      <Flex />
+      <Grid />
+      <Responsive />
+      <Responsive1 />
+    </>
+  )
+}
+
+
+
+export default App
