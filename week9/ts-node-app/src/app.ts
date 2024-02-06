7e9ae8ff39615dbb8d6889b00af1fd3d1959f112
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-06 22:10:02 +0530

    intro to typescript

---
+++ week9/ts-node-app/src/app.ts
@@ -0,0 +1,43 @@
+// function that takes another function as input and runs it after 1 sec
+
+function callAnotheFn(fn: () => void) {         // type of function
+    setTimeout(fn, 1000)
+}
+
+callAnotheFn(() => console.log('hey'))
+
+// function that takes user as input and returns true or false bsaed on age
+//way 1
+function isLegal(user: {
+    firstname: string,
+    lastname: string,
+    age: number
+}): boolean {
+    if (user.age > 18) return true
+    else return false
+}
+
+console.log(isLegal({
+    firstname: 'shubham',
+    lastname: 'wagh',
+    age: 25
+}))
+
+// way 2
+interface User {
+    firstname: string,
+    lastname: string,
+    age: number,
+    email?: string          // optional argument
+}
+
+function isLegal2(user: User) {
+    if (user.age > 18) return true
+    else return false
+}
+
+console.log(isLegal2({
+    firstname: 'shubham',
+    lastname: 'wagh',
+    age: 25
+}))
