c3a86d46dbe8359a4cf2eda842ac6a51724b4791
shubham.wagh <shubhamwagh2544@gmail.com>
2024-02-10 17:27:34 +0530

    business-card complete

--- week5/level-1/business-card/src/App.tsx
+++ week5/level-1/business-card/src/App.tsx
@@ -3,7 +3,43 @@ import { Card } from "./components/Card"
 function App() {
 
   return (
-    <Card />
+    <div style={{
+      display: 'flex',
+      justifyContent: 'flex-start',
+      flexWrap: 'wrap',
+      gap: 25
+    }}>
+      <Card
+        name='shubham wagh'
+        description='software engineer'
+        interests={['coding', 'writing']}
+        socials={['linkedin', 'instagram']}
+      />
+      <Card
+        name='shubham ramesh wagh'
+        description='software engineer'
+        interests={['coding', 'writing']}
+        socials={['linkedin', 'instagram']}
+      />
+      <Card
+        name='shubham wagh'
+        description='software engineer at microsoft india'
+        interests={['coding', 'writing']}
+        socials={['linkedin', 'instagram']}
+      />
+      <Card
+        name='shubham wagh'
+        description='software engineer'
+        interests={['coding', 'writing', 'coding', 'writing', 'coding', 'writing', 'coding', 'writing']}
+        socials={['linkedin', 'instagram']}
+      />
+      <Card
+        name='shubham wagh'
+        description='software engineer'
+        interests={['coding', 'writing', 'coding', 'writing', 'coding', 'writing', 'coding', 'writing']}
+        socials={['linkedin', 'instagram', 'linkedin', 'instagram', 'linkedin', 'instagram']}
+      />
+    </div>
   )
 }
 
--- week5/level-1/business-card/src/components/Card.tsx
+++ week5/level-1/business-card/src/components/Card.tsx
@@ -7,8 +7,50 @@ interface Data {
 
 export function Card(props: Data) {
     return (
-        <>
-
-        </>
+        <div style={{
+            backgroundColor: 'gray',
+            border: '1px solid black',
+            padding: '20px',
+            maxWidth: '300px',
+            minWidth: '200px'
+        }}>
+            <div>
+                <h2 style={{
+                    margin: 0
+                }}>{props.name}</h2>
+                <h3 style={{
+                    marginTop: 5,
+                    fontWeight: 'lighter'
+                }}>{props.description}</h3>
+            </div>
+            <h2 style={{
+                marginTop: 5,
+                marginBottom: 0
+            }}>interests</h2>
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 5,
+                marginTop: 0
+            }}>
+                {props.interests.map((item, index) => (
+                    <h4 style={{ marginTop: 5, marginBottom: 0, fontWeight: 'lighter' }} key={index}>{item}</h4>
+                ))}
+            </div>
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 5,
+                marginTop: 20,
+                marginBottom: 10
+            }}>
+                {
+                    props.socials.map((item, index) => (
+                        <button style={{ backgroundColor: 'burlywood' }}
+                            key={index}>{item}</button>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
