badb591d920abe9ff4eddf95a6ddf38ff9b35ce6
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-10 21:01:38 +0530

    auth middleware logic in hono

--- week10/test-hono-app/src/index.ts
+++ week10/test-hono-app/src/index.ts
@@ -5,14 +5,17 @@ const app = new Hono()
 app.post('/', async (c: any, next: any) => {
   const body = await c.req.json()
   if (c.req.header("Authorization")) {
-    console.log(body);
-    console.log(c.req.query("param"));
+    //console.log(body);
+    //console.log(c.req.query("param"));
+
+    // auth validation
 
     await next()          // middleware
     console.log('will run after next middleware call')
   }
-
-  return c.text('Hello Hono!')
+  else {
+    return c.text('you do not have auth access')
+  }
 })
 
 export default app
