7e30c3ec909c4e894d85e0c7e44a75b6a55775fb
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-11 22:55:36 +0530

    array destructured

---
+++ code-snippets/dist/ArrayDestructure.js
@@ -0,0 +1,7 @@
+"use strict";
+function change() {
+    let arr = [1, 2, 3, 4, 5];
+    let newArr = [...arr, 6, 7, 8, 9, 10];
+    return newArr;
+}
+console.log(change(), '": length: ', change().length);
---
+++ code-snippets/src/ArrayDestructure.ts
@@ -0,0 +1,11 @@
+function change() {
+    let arr = [1, 2, 3, 4, 5]
+
+    let newArr = [...arr, 6, 7, 8, 9, 10]
+
+    return newArr
+}
+
+console.log(
+    change()
+)
