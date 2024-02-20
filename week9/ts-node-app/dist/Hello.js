0dd0312f1c43ef81a79ed07915c0d7b7e388d8ee
shubham.wagh <shubhamwagh2544@gmail.com>
2024-02-20 18:59:59 +0530

    primitive types in ts

--- week9/ts-node-app/dist/Hello.js
+++ week9/ts-node-app/dist/Hello.js
@@ -2,3 +2,7 @@
 let greetings = "Hello, Shubham";
 greetings = greetings.toLowerCase();
 console.log(greetings);
+let userId = 2332.3;
+console.log(userId.toFixed(2));
+let isLoggedIn = false;
+console.log(isLoggedIn);
--- week9/ts-node-app/src/Hello.ts
+++ week9/ts-node-app/src/Hello.ts
@@ -1,5 +1,26 @@
 let greetings: string = "Hello, Shubham"
-
 greetings = greetings.toLowerCase()
-
 console.log(greetings)
+
+
+let userId = 2332.3
+console.log(userId.toFixed(2))
+
+let isLoggedIn: boolean = false
+console.log(isLoggedIn)
+
+
+// any: bad practise (any is used to avoid type-checking errors on particular value)
+let hero                        // let hero: any
+function getHero() {
+    return "hero"
+}
+hero = getHero()
+
+
+// instead add type beforehand
+let hero2: string
+function getHero2(): string {
+    return "hero"
+}
+hero2 = getHero2()
