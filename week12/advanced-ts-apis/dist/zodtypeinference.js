fbe1b16a52690fdcdf8edc2b94ba1c7998c766aa
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-25 18:14:47 +0530

    advanced apis in ts complete

--- week12/advanced-ts-apis/dist/zodtypeinference.js
+++ week12/advanced-ts-apis/dist/zodtypeinference.js
@@ -4,4 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const zod_1 = __importDefault(require("zod"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const ZodStringSchema = zod_1.default.string(); // runtime variable
+// use case
+const UpdateBodySchema = zod_1.default.object({
+    name: zod_1.default.string(),
+    age: zod_1.default.number(),
+    username: zod_1.default.string()
+});
+app.get('/', (req, res) => {
+    //const updateBody: UpdateBodyType = req.body
+    const updateBody = req.body;
+    const { success } = UpdateBodySchema.safeParse(req.body);
+    // logic
+    return res.status(200).json({
+        message: 'Success!'
+    });
+});
+app.listen(3000, () => { console.log('Server is running on port 3000'); });
