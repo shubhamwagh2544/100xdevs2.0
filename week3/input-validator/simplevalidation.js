e111fdcc1ab5d45dc9650efa829ef9a96b2fe61c
shubham.wagh <shubhamwagh2544@gmail.com>
2024-02-28 00:29:21 +0530

    input validation with zod and zod infer

--- week3/input-validator/simplevalidation.js
+++ week3/input-validator/simplevalidation.js
@@ -17,3 +17,26 @@ inputValidator({
     number: 25,
     bool_array: [true, false]
 });
+
+
+function inputValidator2({ username, password }) {
+    const schema2 = zod.object({
+        username: zod.string().email(),
+        firstname: zod.string(),
+        lastname: zod.string(),
+        password: zod.string()
+    })
+
+    const res = schema2.safeParse({ username, password })
+    if (!res.success) {
+        console.log('success')
+    }
+    else {
+        console.log('failure')
+    }
+}
+
+inputValidator2({
+    username: 'shubhamwagh@gmail.com',
+    password: 'shubhamwagh'
+})
