6feb1875e0af41b3d89cbcc8f738557af42e0b35
shubham.wagh <shubhamwagh2544@gmail.com>
2024-03-03 19:42:22 +0530

    getting backend data in next app

---
+++ week14/simple-next-app/app/test-backend/page.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+async function getUsersData() {
+    // delay for 5 sec
+    await new Promise((r) => setTimeout(r, 5000))
+
+    const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details')
+    return response.data
+}
+
+// async component
+export default async function User() {
+    const userDetails = await getUsersData()
+
+    return (
+        <div className="flex flex-col justify-center h-screen">
+            <div className="flex justify-center">
+                <div className="border p-8 rounded">
+                    <div>
+                        Name: {userDetails?.name}
+                    </div>
+
+                    {userDetails?.email}
+                </div>
+            </div>
+        </div>
+    )
+}
