ede13ca2ecf290403d95cc3a56d2654b5fd0b4b8
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-04-15 22:50:50 +0530

    next server talking to next backend

--- week14/nextbackend-in-action/app/page.tsx
+++ week14/nextbackend-in-action/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import axios from "axios";
+
+async function getUserData() {
+  //const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  const response = await axios.get("http://localhost:3000/api/user")
+  return response.data
+}
+
+// async component
+export default async function Home() {
+  const userdetails = await getUserData()
   return (
-    <div>
-      <h1>Next.js + NextBackend</h1>
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userdetails?.name}
+                </div>
+                
+                {userdetails?.email}
+            </div>
+        </div>
     </div>
   );
 }
