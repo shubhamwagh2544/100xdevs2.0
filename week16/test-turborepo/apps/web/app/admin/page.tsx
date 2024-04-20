4b32fc0931a13efb8020e0698ed3a3230313e175
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-04-20 09:45:00 +0530

    added admin component in shared ui repo to use it in web app

---
+++ week16/test-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import Admin from "@repo/ui/admin";
+
+export default function () {
+    return (
+        <>
+            <h2>Hi From Admin Route</h2>
+            <Admin />
+        </>
+    )
+}
