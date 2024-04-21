27987f193c0ddf5eee11400c84d256b53b18014c
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-04-21 12:04:42 +0530

    github auth in next

--- week16/nextauth/app/lib/auth.ts
+++ week16/nextauth/app/lib/auth.ts
@@ -1,5 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github"
 
 export const NEXT_AUTH = {
     providers: [
@@ -29,6 +30,10 @@ export const NEXT_AUTH = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
         })
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
