5be78a59f5237a1755e89404dbc4a61eb23f46e0
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-05-15 09:31:51 +0530

    revalidating cache in ssg

--- week19/test-ssr-ssg/app/page.tsx
+++ week19/test-ssr-ssg/app/page.tsx
@@ -1,13 +1,18 @@
+import revalidate from "./lib/actions/action1"
+
 export default async function Home() {
   const res = await fetch("https://sum-server.100xdevs.com/todos", {
     next: {
-      revalidate: 10                          //
+      //revalidate: 10                          // revalidate the cache every 10 sec
+      tags: ['todos']
     }
   })
   const data = await res.json()
 
   console.log('data found on server is: ')
   console.log(JSON.stringify(data))           // generated as static content
+
+  revalidate();
 
   return (
     <div>
