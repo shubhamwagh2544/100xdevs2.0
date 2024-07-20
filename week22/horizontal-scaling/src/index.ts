fad313b6975fdf58dd000301c003dd5f4d2df41b
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-07-20 10:12:12 +0530

    horizontal scaling nodejs

---
+++ week22/horizontal-scaling/src/index.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import cluster from 'cluster';
+import os from 'os';
+
+const totalCPUs = os.cpus().length;
+const port = 3000;
+
+if (cluster.isPrimary) {
+    console.log(`Number of CPUs is ${totalCPUs}`);
+    console.log(`Primary ${process.pid} is running`);
+
+    // fork workers
+    for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died: `, code);
+        console.log("Let's fork another worker!");
+        cluster.fork();
+    })
+}
+else {
+    const app = express();
+    console.log(`Worker ${process.pid} started`);
+
+    app.get("/api/:n", function (req, res) {
+        let n = parseInt(req.params.n);
+        let count = 0;
+    
+        if (n > 5000000000) n = 5000000000;
+    
+        for (let i = 0; i <= n; i++) {
+          count += i;
+        }
+    
+        res.send(`Final count is ${count} ${process.pid}`);
+      });
+    
+      app.listen(port, () => {
+        console.log(`App listening on port ${port}`);
+      });
+}
+
+/*
+Horizontal Scaling: 
+Creating multiple instances of nodejs application, distributing the load across multiple servers.
+
+Bad for two reasons: 
+1. Just ugly to do this, keep track of the processes that are up and down
+2. Processes will have port conflicts, you’ll have to run each process on a saparate port
+*/
