67d0123030221564a3ccb142abfedf4b9eef08b6
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-07-21 14:35:59 +0530

    webrtc-frontend+backend

--- week23/webrtc/backend/src/index.ts
+++ week23/webrtc/backend/src/index.ts
@@ -10,10 +10,13 @@ wss.on("connection", function connection(ws) {
   ws.on("message", function message(data: any) {
     const message = JSON.parse(data);
     if (message.type === "sender") {
+      console.log("sender set");
       senderSocket = ws;
     } else if (message.type === "receiver") {
+      console.log("receiver set");
       receiverSocket = ws;
     } else if (message.type === "createOffer") {
+      console.log("offer received");
       if (ws !== senderSocket) {
         return;
       }
@@ -21,6 +24,7 @@ wss.on("connection", function connection(ws) {
         JSON.stringify({ type: "createOffer", sdp: message.sdp })
       );
     } else if (message.type === "createAnswer") {
+      console.log("answer set");
       if (ws !== receiverSocket) {
         return;
       }
@@ -29,10 +33,12 @@ wss.on("connection", function connection(ws) {
       );
     } else if (message.type === "iceCandidate") {
       if (ws === senderSocket) {
+        console.log("sender icecandidate set");
         receiverSocket?.send(
           JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
         );
       } else if (ws === receiverSocket) {
+        console.log("receiver icecandidate set");
         senderSocket?.send(
           JSON.stringify({ type: "iceCandidate", candidate: message.candidate })
         );
